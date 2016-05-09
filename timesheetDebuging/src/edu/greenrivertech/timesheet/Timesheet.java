90886cd68784a35e7327729f56965cf6db67be1d
yegorshem <yshemereko@mail.greenriver.edu>
2016-05-09 12:42:25 -0700

    added week inner class, modified methods and method names

--- timesheetDebuging/src/edu/greenrivertech/timesheet/Timesheet.java
+++ timesheetDebuging/src/edu/greenrivertech/timesheet/Timesheet.java
@@ -5,92 +5,135 @@ import java.util.List;
 
 public class Timesheet
 {
+	
 	private static final int DAYS = 7;
 	private static final int MAX_HOURS = 40;
+
+	private class Week
+	{
+		private int totalHours;
+		private int totalPay;
+		int[] days = new int[DAYS];
+	}
 	
+	//fields
 	private int numberOfWeeks;
-	private int totalHours;
-	private int totalPay;
+	private Week tempWeek;
+	List<Week> listOfWeeks = new ArrayList<>();
 	
-	int[] week = new int[DAYS];
-	List<int[]> listOfTimesheets = new ArrayList<>();
+
+	/**
+	 * Constructor
+	 */
+	public Timesheet()
+	{
+		this.numberOfWeeks = 0;
+		// this.totalHours = 0;
+		// this.totalPay = 0;
+	}
+
+	
+	//main methods
 	
 	/**
-	 * Constructor 
+	 * For each week inside the timesheet object, it calculates what the total
+	 * pay for that week is
 	 */
-	public Timesheet(){
-		this.numberOfWeeks = 0;
-		this.totalHours = 0;
-		this.totalPay = 0;
-	}
-	
-	public void setListofTimesheets(int[] newTimesheet){
-		this.listOfTimesheets.add(newTimesheet);
-	}
-	
-	public void calculatePay(){
-		for(int[] singleWeek: this.listOfTimesheets){
+	public void calculatePay()
+	{
+		for (Week singleWeek : this.listOfWeeks)
+		{
 			int maxHrs = MAX_HOURS;
-			for (int i = 0; i < singleWeek.length; i++)
+			for (int i = 0; i < singleWeek.days.length; i++)
 			{
 				double tempDailyPay = 0;
 				// Checks if worked and calculates base pay
-		        if(singleWeek[i] > 0)
-		        {
-		            tempDailyPay = singleWeek[i]*10;
-		        }
+				if (singleWeek.days[i] > 0)
+				{
+					tempDailyPay = singleWeek.days[i] * 10;
+				}
 
-		        // If work over 8 hours and calc additional 2 dollars
-		        if(singleWeek[i]> 8)
-		        {
-		            tempDailyPay += (singleWeek[i] - 8) * 2;
-		        }
+				// If work over 8 hours and calc additional 2 dollars
+				if (singleWeek.days[i] > 8)
+				{
+					tempDailyPay += (singleWeek.days[i] - 8) * 2;
+				}
 
-		        // Starts with 40 hour and then sets max hours to hours already calculated.
-		        if(this.totalHours > maxHrs)
-		        {
-		            tempDailyPay += (totalHours - maxHrs)*4;
-		            maxHrs = totalHours;
-		        }
+				// Starts with 40 hour and then sets max hours to hours already
+				// calculated.
+				if (singleWeek.totalHours > maxHrs)
+				{
+					tempDailyPay += (singleWeek.totalHours - maxHrs) * 4;
+					maxHrs = singleWeek.totalHours;
+				}
 
-		        // Calculated time and a half for Sunday
-		        if(i == 0)
-		        {
-		            tempDailyPay *= 1.5;
-		        }
+				// Calculated time and a half for Sunday
+				if (i == 0)
+				{
+					tempDailyPay *= 1.5;
+				}
 
-		        // Calculated double time for Saturday
-		        if(i == 6)
-		        {
-		            tempDailyPay *= 2.0;
-		        }
-		        this.totalPay += tempDailyPay;
+				// Calculated double time for Saturday
+				if (i == 6)
+				{
+					tempDailyPay *= 2.0;
+				}
+
+				singleWeek.totalPay += tempDailyPay;
 			}
 		}
 	}
 	
+	//Getters and Setters
+
 	/**
 	 * 
 	 * @param numberOfWeeks
 	 */
-	public void setNumberOfWeeks(int numberOfWeeks){
+	public void setNumberOfWeeks(int numberOfWeeks)
+	{
 		this.numberOfWeeks = numberOfWeeks;
 	}
-	
+
 	/**
 	 * 
 	 * @return
 	 */
-	public int getNumberOfWeeks(){
+	public int getNumberOfWeeks()
+	{
 		return this.numberOfWeeks;
 	}
-	
-	public void addHour(int day, int hours){
-		this.week[day] = hours;
+
+	/**
+	 * 
+	 * @param day
+	 * @param hours
+	 */
+	public void addHour(int day, int hours)
+	{
+		this.tempWeek.days[day] = hours;
 	}
-	
-	public int[] getWeek(){
-		return this.week;
+
+	/**
+	 * 
+	 * @param newWeek
+	 */
+	public void addWeek(Week newWeek)
+	{
+		this.listOfWeeks.add(newWeek);
 	}
+
+
+	public Week getTempWeek()
+	{
+		return tempWeek;
+	}
+
+
+
+
+
 	
+	
+
 }
